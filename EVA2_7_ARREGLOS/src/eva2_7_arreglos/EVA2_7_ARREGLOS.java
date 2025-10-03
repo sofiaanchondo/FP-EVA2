087a5205cd038486196baf72585cf02dd0473842
sofiaanchondo <anchondosofia78@gmail.com>
2025-10-03 15:52:39 -0700

    Add files via upload

---
+++ EVA2_7_ARREGLOS/src/eva2_7_arreglos/EVA2_7_ARREGLOS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_7_arreglos;
+
+/**
+ *
+ * @author mikas
+ */
+public class EVA2_7_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cade = "Hola mundo!!";
+        System.out.println(cade.charAt(0));
+        char caracter = cade.charAt(11);
+        System.out.println(caracter);
+        System.out.println(cade.length());
+        
+          
+    }
+    
+}
---
+++ EVA2_8_FOR/src/eva2_8_for/EVA2_8_FOR.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_for;
+
+/**
+ *
+ * @author mikas
+ */
+public class EVA2_8_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //3 PARAMETROS
+// variable y valor de inicio
+// hasta donde llegas
+// cuando es falso es for se detiene
+// incremento o decremento
+// sumar o restar:
+// operadores: añadir 1 o restar
+// ++ o --
+// i++ significa sumarlen 1 al valor de la i
+        for(int i = 1; i <= 10; i++ ){
+             System.out.println("Hola mundo!!");
+        }
+        // i-- significa restar 1
+         for(int i = 10; i >= 1; i-- ){
+             System.out.println("Hola mundo!!");
+          
+             
+        }
+    }
+    
+}
