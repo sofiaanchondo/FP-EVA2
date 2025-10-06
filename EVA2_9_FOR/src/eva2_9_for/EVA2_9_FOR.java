01d88310a5097fb8b1c781c2f5a9520593e52f55
sofiaanchondo <anchondosofia78@gmail.com>
2025-10-06 16:52:49 -0600

    Add files via upload

---
+++ EVA2_9_FOR/src/eva2_9_for/EVA2_9_FOR.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_9_for;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sofia
+ */
+public class EVA2_9_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner captu = new Scanner(System.in);
+        int rep;
+        String mensaje;
+        System.out.println("Mensaje a repetir");
+        mensaje = captu.nextLine();
+        System.out.println("Cantidad de repeticiones: ");
+        rep = captu.nextInt();
+        for(int i = 1; i <= rep; i++){
+        System.out.println(mensaje);
+            
+            
+        }
+        }
+    }
+    
+
