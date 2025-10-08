10d2c6df0341eee98870b82a95797b296f818cfd
sofiaanchondo <anchondosofia78@gmail.com>
2025-10-08 16:50:19 -0600

    Add files via upload

---
+++ EVA2_14_CAPTURA/src/eva2_14_captura/EVA2_14_CAPTURA.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_captura;
+import java.util.Scanner;
+/**
+ *
+ * @author Sofia
+ */
+public class EVA2_14_CAPTURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner captu = new Scanner (System.in);  
+    int tama;
+    System.out.println("Cantidad de calificaciones: ");
+    tama = captu.nextInt();
+    int califa[] = new int [tama];
+    //CAPTURAR
+    for(int i = 0; i < califa.length; i++){
+    System.out.println("Calificacion " + (i+1) + ":");
+    califa[i] = captu.nextInt();
+    }
+    //LEER
+    for(int i = 0; i < califa.length; i++){
+    System.out.println("[" + califa[i] + "]");
+    
+    
+    
+        
+        
+        
+        
+    }
+    }
+        
+        
+        
+    }
+    
+
